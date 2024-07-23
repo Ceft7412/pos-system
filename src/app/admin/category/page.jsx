d4cf6d5b25b57421f40b62cd1dcf1dcf5c917191
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-23 19:32:39 +0800

    Create page.jsx

---
+++ src/app/admin/category/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import MainLayout from "@/layouts/MainLayout";
+export default function Category() {
+  return (
+    <MainLayout>
+      <div>Category</div>
+    </MainLayout>
+  );
+}
