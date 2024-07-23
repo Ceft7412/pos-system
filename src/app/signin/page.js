5432f218b133d54af68927c21c25c5a0630fd1fa
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-23 19:33:03 +0800

    Create page.js

---
+++ src/app/signin/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+import GuestLayout from "@/layouts/GuestLayout";
+export default function Signin() {
+  return (
+    <GuestLayout>
+      <div>Sign</div>;
+    </GuestLayout>
+  );
+}
